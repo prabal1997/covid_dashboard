613fac15e9db3734ff2a949b78e19448669909ab
Prabal Gupta <prabal.gupta.1997@ieee.org>
2020-12-15 18:24:34 -0800

    integrated news API, cleaned up UI, allowed choosing heatmap metric

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import altair as alt
 from vega_datasets import data
 import iso3166
 import argparse
+from newsapi import NewsApiClient
 
 CONTINENTS = ("World", "Asia", "North America", "South America", "Africa", "Europe", "Oceania",)
 CONTINENTS_EMOJI = ("🗺️", "🌏", "🌎", "🌎", "🌍", "🌍", "🌏",)
@@ -77,6 +78,82 @@ def fetch_covid_data(DATA_URL="https://covid.ourworldindata.org/data/owid-covid-
 
     return {"map": map_covid_data, "linechart": linechart_covid_data}
 
+
+def cache_covid_news(covid_data_map_full,
+                     NEWS_API_KEY,                     
+                     start_date=(pd.Timestamp.today() - pd.Timedelta("7d")).strftime("%Y-%m-%d")):
+    
+    # fetch covid news relevant to specific countries
+    def fetch_covid_news(continent="", country_list=[]):
+        # Init
+        newsapi = NewsApiClient(api_key=NEWS_API_KEY)
+        if (len(continent)):
+            country_list = list(country_list) + [continent]
+        country_list = list(country.lower() for country in country_list)
+
+        # fetch news within past week
+        SEARCH_KEYWORDS = ("corona", "covid", "pandemic", "coronavirus", "covid19", "covid-19")
+        search_keywords_query = "({})".format(" OR ".join("\"{}\"".format(element) for element in SEARCH_KEYWORDS))
+        search_country_query =  "({})".format(" OR ".join("\"{}\"".format(element) for element in country_list))
+        if (len(country_list)):
+            query = "{0} AND {1}".format(search_keywords_query, search_country_query)
+            sort_method = 'relevancy'
+        else:
+            query = search_keywords_query
+            sort_method = 'relevancy'
+
+        try:
+            @st.cache
+            def call_news_api(query, start_date, sort_method):
+                # return {'status': "ok",
+                #         'totalResults': 2,
+                #         'articles': [{'title': "COVID coronavirus asia north america canada is in the title!",
+                #                       'description': "Some news description",
+                #                       'content': "A looooong news article",
+                #                       'url': "https://www.google.com",
+                #                       "author": "My Name",
+                #                       "source": {"id": "news_company",
+                #                                  "name": "News Company"}}
+                #                      ]*2}
+
+                return newsapi.get_everything(q=query,
+                                              from_param=start_date,
+                                              sort_by=sort_method,
+                                              language='en')
+            api_response = call_news_api(query, start_date, sort_method)
+
+            if ((api_response['status'] == 'ok') and (api_response['totalResults'])):
+                news_list = api_response['articles']
+                
+                def keywords_in_news_article(news_article_input, search_keywords_list):
+                    combined_text = (str(news_article_input['title']) + str(news_article_input['description']) + str(news_article_input['content'])).lower()
+                    contains_some_keywords = any((search_keyword in combined_text) for search_keyword in search_keywords_list)     
+                    
+                    return contains_some_keywords
+
+                news_list = list(news_article for news_article in news_list if (keywords_in_news_article(news_article, SEARCH_KEYWORDS)))
+                if (len(country_list)):
+                    news_list = list(news_article for news_article in news_list if (keywords_in_news_article(news_article, country_list)))                
+                else:
+                    pass
+                    # st.write(news_list)
+
+                return news_list
+            
+        except Exception as e:
+            print(e)
+        return []
+    
+    # fetch covid news for each continent, and for the world
+    covid_continet_countries_dict = covid_data_map_full[["continent", "location"]].drop_duplicates().groupby('continent').apply(lambda x: tuple(x['location'])).to_dict()
+    covid_continent_news_dict = dict()
+    for continent in covid_continet_countries_dict:
+        covid_continent_news_dict[continent] = fetch_covid_news(continent=continent,
+                                                                country_list=covid_continet_countries_dict[continent])
+    covid_continent_news_dict["World"] = fetch_covid_news()
+        
+    return covid_continent_news_dict
+
 # parse command line arguments to receive information about NEWS API key
 parser = argparse.ArgumentParser(description='This webapp serves as a simple COVID19 dashboard')
 
@@ -92,6 +169,8 @@ except SystemExit as e:
     os._exit(e.code)
 NEWS_API_KEY = args.news_api_key
 
+
+
 # add a title
 st.title("🦠 Coronavirus Dashboard")
 
@@ -100,7 +179,7 @@ selectbox_options = list("{0} {1}".format(name, emoji) for name, emoji in zip(CO
 def selectbox_option_to_location(input_selectbox_option):
     return input_selectbox_option[:input_selectbox_option.rfind(' ')]
 
-st.header("Coronavirus Daily Status")
+st.header("📈 Pandemic Trend")
 location_selectbox = st.selectbox("Please Choose a Region", selectbox_options)
 
 # fetch data relevant to the location, sort by date
@@ -108,16 +187,17 @@ covid_data = fetch_covid_data()
 location = selectbox_option_to_location(location_selectbox)
 
 IS_WORLD = (location == "World")
+covid_data_map_full = covid_data["map"] 
 if not(IS_WORLD):
     covid_data_query = ("continent == '{0}'".format(location))
     covid_data_linechart = covid_data["linechart"].query(covid_data_query)
 
-    covid_data_map = covid_data["map"].query(covid_data_query)
+    covid_data_map = covid_data_map_full.query(covid_data_query)
 else:
     covid_data_linechart = covid_data["linechart"].copy(deep=True)
     covid_data_linechart = covid_data_linechart.groupby("date").apply(calculate_linechart_stats).reset_index()
 
-    covid_data_map = covid_data["map"].copy(deep=True)
+    covid_data_map = covid_data_map_full.copy(deep=True)
 
     # remove incorrect data-points that do not include every nation
     MIN_POPULATION_THRESHOLD = 7.5 * (10**9)
@@ -169,12 +249,25 @@ line = alt.Chart(covid_data_linechart).mark_line(
 ).encode(
     x=x_axis,
     y=y_smooth_axis
-)
+).interactive()
 
 # combine the scatter, line plot and show on the screen
 st.altair_chart((scatter_plot + line).configure_view(strokeWidth=2), use_container_width=True)
 
 # make a map in Altair
+st.header("🌡 Pandemic Heatmap")
+
+TOTAL_CASES_LABEL = "Total Cases Per Million"
+TOTAL_DEATHS_LABEL = "Total Deaths Per Million"
+chosen_metric = st.selectbox("Please Choose a Metric", (TOTAL_CASES_LABEL, TOTAL_DEATHS_LABEL,))
+
+if (chosen_metric == TOTAL_CASES_LABEL):
+    metric_column_name = "total_cases_smoothed_per_million"
+    scale = "linear"    
+else:
+    metric_column_name = "total_deaths_smoothed_per_million"    
+    scale = "linear"
+
 tooltip = [alt.Tooltip("location:N", title="Country"),
            alt.Tooltip("total_cases_smoothed:Q", title="Total Cases", format=",r"),
            alt.Tooltip("total_deaths_smoothed:Q", title="Total Deaths", format=",r"),
@@ -183,9 +276,9 @@ tooltip = [alt.Tooltip("location:N", title="Country"),
 
 countries = alt.topo_feature(data.world_110m.url, 'countries')
 foreground = alt.Chart(countries).mark_geoshape(stroke="white", strokeWidth=0.30
-).encode(color=alt.Color('total_cases_smoothed_per_million:Q', 
-                        scale=alt.Scale(scheme="turbo", type="log"), title="Total Cases Per Million", legend=alt.Legend(orient="left")),
-         tooltip=tooltip,
+).encode(color=alt.Color('{}:Q'.format(metric_column_name), 
+                        scale=alt.Scale(scheme="reds", type="linear"), title=chosen_metric, legend=alt.Legend(orient="top")),
+                        tooltip=tooltip,
 ).transform_lookup(
     lookup='id',
     from_=alt.LookupData(covid_data_map, 'id', ['total_cases_smoothed', 'total_deaths_smoothed', 'total_cases_smoothed_per_million', 'total_deaths_smoothed_per_million', 'continent', 'location'])
@@ -198,3 +291,34 @@ final_map = (
     .project(**CONTINENTS_ROTATION[location])
 )
 st.altair_chart(final_map, use_container_width=True)
+
+# get news about COVID
+def show_news(title, url, author, news_agency, urlToImage):
+    st.subheader("[{title}]({url})".format_map({"title": title,
+                                                "url": url}))
+    if (author):
+        st.write("by **{author}** in **{news_agency}**".format_map({"author": author,
+                                                                    "news_agency": news_agency}))
+    else:
+        st.write("by **{news_agency}**".format_map({"news_agency": news_agency}))
+
+covid_news_cache = cache_covid_news(covid_data_map_full, NEWS_API_KEY)
+news_articles = covid_news_cache[location]
+
+if (len(news_articles)):
+    # show title, source name, and URL
+    st.header("📰 Regional News")
+    SHOWN_TITLES = set()
+
+    # show articles with unique headlines
+    MAX_ARTICLE_COUNT = 5
+    for news_article in news_articles[:MAX_ARTICLE_COUNT]:
+        title = news_article['title']
+        url = news_article['url']
+        author = news_article['author']
+        news_agency = news_article['source']['name']
+        urlToImage = news_article['urlToImage']
+
+        if (title not in SHOWN_TITLES):
+            show_news(title, url, author, news_agency, urlToImage)
+            SHOWN_TITLES.add(title) 
